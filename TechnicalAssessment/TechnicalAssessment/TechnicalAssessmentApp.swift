054ae247a68e87c8d9ea89747f6820a6ff53d049
Kacper Harezga <kacperharezga@gmail.com>
2023-12-10 23:06:53 +0100

    Create project seed

---
+++ TechnicalAssessment/TechnicalAssessment/TechnicalAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnicalAssessmentApp.swift
+//  TechnicalAssessment
+//
+//  Created by Harezga Kacper on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TechnicalAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
